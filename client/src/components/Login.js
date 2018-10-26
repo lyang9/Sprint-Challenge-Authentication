bc2c0438201cee1b0d096f847a8cc58fe2ba9f4e
lyang9 <lyang9@buffalo.edu>
2018-10-26 18:24:35 -0400

    Imported Form.css

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './Login.css';
+import './Form.css';
 
 class Login extends Component {
   state = {
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './Form.css';
 
 class Register extends Component {
   state = {
