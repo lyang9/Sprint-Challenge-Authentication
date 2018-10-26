2f781ececb235515bad4175c3dd93b2a437bce1f
lyang9 <lyang9@buffalo.edu>
2018-10-26 16:07:42 -0400

    Imported jokes and rendered NavLink and Route

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { NavLink, Route } from 'react-router-dom';
 import Home from './components/Home';
-
+import Jokes from './components/Jokes';
 import Login from './components/Login';
 
 class App extends Component {
@@ -12,13 +12,14 @@ class App extends Component {
         <header className="App-header">
           <nav>
             <NavLink exact to='/'>Home</NavLink>
-            
+            &nbsp;|&nbsp;
+            <NavLink to='/jokes'>Jokes</NavLink>
             &nbsp;|&nbsp;
             <NavLink to='/login'>Login</NavLink>
           </nav>
           <main>
             <Route exact path='/' component={Home} />
-            
+            <Route path='/jokes' component={Jokes} />
             <Route path='/login' component={Login} />
           </main>
         </header>
