84057c58f38407ee9cc3475f76b8e32228775116
lyang9 <lyang9@buffalo.edu>
2018-10-26 14:03:00 -0400

    Imported login from components folder

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { NavLink, Route } from 'react-router-dom';
 import Home from './components/Home';
 
+import Login from './components/Login';
+
 class App extends Component {
   render() {
     return (
@@ -10,9 +12,14 @@ class App extends Component {
         <header className="App-header">
           <nav>
             <NavLink exact to='/'>Home</NavLink>
+            
+            &nbsp;|&nbsp;
+            <NavLink to='/login'>Login</NavLink>
           </nav>
           <main>
             <Route exact path='/' component={Home} />
+            
+            <Route path='/login' component={Login} />
           </main>
         </header>
       </div>
