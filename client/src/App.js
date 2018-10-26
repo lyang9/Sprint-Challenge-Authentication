d9f8d38ac3a0517b7884f88a282455fe141ddb6d
lyang9 <lyang9@buffalo.edu>
2018-10-26 16:37:48 -0400

    Imported Logout and rendered NavLink and Route

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { NavLink, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Jokes from './components/Jokes';
 import Login from './components/Login';
+import Logout from './components/Logout';
 
 class App extends Component {
   render() {
@@ -16,11 +17,14 @@ class App extends Component {
             <NavLink to='/jokes'>Jokes</NavLink>
             &nbsp;|&nbsp;
             <NavLink to='/login'>Login</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink onClick={this.handleLogout} to='/logout'>Logout</NavLink>
           </nav>
           <main>
             <Route exact path='/' component={Home} />
             <Route path='/jokes' component={Jokes} />
             <Route path='/login' component={Login} />
+            <Route path='/logout' component={Logout} />
           </main>
         </header>
       </div>
