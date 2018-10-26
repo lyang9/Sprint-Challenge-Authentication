cc268e8fd3370cb8ae18a4c8c6ede3efa037b9fb
lyang9 <lyang9@buffalo.edu>
2018-10-26 12:34:14 -0400

    pulled in jsonwebtoken and jwtKey. Completed login function

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Import middleware
 const { authenticate } = require('./middlewares');
@@ -32,6 +33,8 @@ function register(req, res) {
     })
 };
 
+const jwtKey = require('../_secrets/keys').jwtKey;
+
 function generateToken(user) {
   const jwtPayload = {
     ...user,
@@ -40,12 +43,28 @@ function generateToken(user) {
     expiresIn: '3h',
   };
 
-  return jwt.sign(jwtPayload, jwtSecret, jwtOptions);
+  return jwt.sign(jwtPayload, jwtKey, jwtOptions);
 };
 
 function login(req, res) {
   // implement user login
-}
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user); // generate token for specific user
+        res.status(200).json({ welcome: user.username, token });
+      } else {
+        res.status(401).json({ message: 'username or password is incorrect' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Unable to login', err });
+    })
+};
 
 function getJokes(req, res) {
   axios
